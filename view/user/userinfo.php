342527c5889d3a49edb8cd893c8c388b38eee57c
vananhbui <vananhbui41@gmail.com>
2021-12-20 17:25:38 +0700

    đưa user name ra center

--- view/user/userinfo.php
+++ view/user/userinfo.php
@@ -5,7 +5,7 @@
                 <div class="user-avatar">
                     <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin">
                 </div>
-                <h5 class="user-name"><?php echo $_SESSION['userInf']['HoTen']; ?></h5>
+                <center><h5 class="user-name"><?php echo $_SESSION['userInf']['HoTen']; ?></h5></center>
                 <!-- <h6 class="user-email">vananhbui41@gmail.com</h6> -->
             </div>
         </div>
