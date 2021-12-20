4ea00603aceefb8dfa7134778fe09f5d02a1006c
ChuHien <chuhien4220na@gmail.com>
2021-12-20 17:07:39 +0700

    admin

---
+++ controller/updateProfile.php
@@ -0,0 +1,22 @@
+<?php
+//Khai báo sử dụng session
+        require("../../model/database.php");
+        if (!isset($_POST['Update'])){
+          die('');
+        }
+        $HoTen = $_POST['HoTen'];
+        $GioiTinh = $_POST['GioiTinh'];
+        $DiaChi = $_POST['DiaChi'];
+        $NgaySinh = $_POST['NgaySinh'];
+        if (!$HoTen|| !$GioiTinh || !$DiaChi || !$NgaySinh){
+        echo "Vui lòng nhập đầy đủ thông tin";
+        } else {
+        $MaKH = $_SESSION['UserInf'][MaKH];
+        $sql = "UPDATE KhachHang SET HoTen='{$HoTen}', GioiTinh={'$GioiTinh}, DiaChi='{$DiaChi}', NgaySinh='{$NgaySinh}' WHERE MaKH=$MaKH";
+        if (mysqli_query($conn, $sql)) {
+            echo "Update thành công";
+        } else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+       }
+?>
--- view/user/quanLyThe.php
+++ view/user/quanLyThe.php
@@ -23,8 +23,8 @@
                         <div class="user-avatar">
                             <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin">
                         </div>
-                        <h5 class="user-name">Bui Van Anh</h5>
-                        <h6 class="user-email">vananhbui41@gmail.com</h6>
+                        <h5 class="user-name"><?php echo $_SESSION['userInf']['HoTen']; ?></h5>
+                        <h6 class="user-sđt"><?php echo $_SESSION['userInf']['SĐT']; ?></h6>
                     </div>
                 </div>
             </div>
