eb68ff05aa7542ebb404f055c72709dcf644cb0f
vananhbui <vananhbui41@gmail.com>
2021-12-20 17:52:06 +0700

    userinfo.php update

--- view/user/userinfo.php
+++ view/user/userinfo.php
@@ -2,11 +2,13 @@
     <div class="card-body">
         <div class="account-settings">
             <div class="user-profile">
+                <center>
                 <div class="user-avatar">
                     <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Maxwell Admin">
                 </div>
                 <h5 class="user-name"><?php echo $_SESSION['userInf']['HoTen']; ?></h5>
                 <h6><?php echo $_SESSION['userInf']['SĐT']; ?></h6>
+                </center>
             </div>
         </div>
     </div>
